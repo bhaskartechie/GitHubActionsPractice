81fafd00b73ad18d6ff1d7226f6bdda82723e0f7
Bhaskar R <bhaskar.techiepro@gmail.com>
2024-03-17 16:19:51 +0530

    initial setup for github actions

--- demo_github_actions/githubactions/tests.py
+++ demo_github_actions/githubactions/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SampleTestCase(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
